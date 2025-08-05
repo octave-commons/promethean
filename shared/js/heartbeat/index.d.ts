4b0d45115d1285c9fd5d84a31545f90e10576eb2
Aaron Beavers <foamy125@gmail.com>
2025-08-05 16:16:54 -0500

    Add heartbeat typings and fix embedding function

---
+++ shared/js/heartbeat/index.d.ts
@@ -0,0 +1,13 @@
+export interface HeartbeatOptions {
+  url?: string;
+  pid?: number;
+  name?: string;
+  interval?: number;
+}
+
+export class HeartbeatClient {
+  constructor(options?: HeartbeatOptions);
+  sendOnce(): Promise<any>;
+  start(): void;
+  stop(): void;
+}
