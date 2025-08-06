5959c00dce1bc450f637a0228baa5aff7fd6f4c1
Aaron Beavers <foamy125@gmail.com>
2025-08-06 14:46:05 -0500

    remove old file watcher tests

--- bridge/events/index.js
+++ bridge/events/index.js
@@ -7,12 +7,9 @@ const events = JSON.parse(
   readFileSync(path.join(__dirname, "events.json"), "utf8"),
 );
 
-export const EventName = {
-  STT_INPUT: events.STT_INPUT.name,
-  STT_OUTPUT: events.STT_OUTPUT.name,
-  CEPHALON_ROUTE: events.CEPHALON_ROUTE.name,
-  TTS_OUTPUT: events.TTS_OUTPUT.name,
-};
+export const EventName = Object.fromEntries(
+  Object.entries(events).map(([key, value]) => [key, value.name]),
+);
 
 export const EVENT_TO_PROTOCOL = Object.fromEntries(
   Object.entries(events).map(([key, value]) => [key, value.protocol || null]),
