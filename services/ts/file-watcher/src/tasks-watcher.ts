e16d8540a8c3c729fe2895a0acff7739dacd1292
Error <foamy125@gmail.com>
2025-08-07 12:23:15 -0500

    Removed leftover garbage from file watcher

--- services/ts/file-watcher/src/tasks-watcher.ts
+++ services/ts/file-watcher/src/tasks-watcher.ts
@@ -18,40 +18,13 @@ export interface TasksWatcherOptions {
 export function createTasksWatcher({
   tasksPath,
   publish,
-  runPython,
-  callLLM,
-  writeFile,
-  mongoCollection,
   socket,
-  maxConcurrentLLMTasks = 1,
 }: TasksWatcherOptions) {
   const watcher = chokidar.watch(tasksPath, { ignoreInitial: true });
-
-  const queue: string[] = [];
-  let active = 0;
-
-  async function process(path: string) {
-    active++;
-    try {
-      await runPython?.(path);
-      const content = await callLLM?.(path);
-      if (content) await writeFile?.(path, content);
-      await mongoCollection?.updateOne?.({}, {} as any);
-      socket?.emit?.("task-processed", { path });
-    } finally {
-      active--;
-      if (queue.length > 0) {
-        process(queue.shift()!);
-      }
-    }
-  }
 
   watcher.on("add", (path) => {
     publish(EVENTS.add, { path });
     queue.push(path);
-    if (active < maxConcurrentLLMTasks) {
-      process(queue.shift()!);
-    }
   });
 
   watcher.on("change", (path) => {
