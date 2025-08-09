29ee464f97180a34738658105959b5a52f72c86f
Aaron Beavers <foamy125@gmail.com>
2025-08-09 18:03:29 -0500

    feat: add generic graph data structure

--- shared/js/prom-lib/ds/graph.test.ts
+++ shared/js/prom-lib/ds/graph.test.ts
@@ -3,17 +3,18 @@ import { Graph } from "./graph";
 describe("Graph", () => {
   test("bfs traverses undirected graph", () => {
     const g = new Graph({ directed: false });
-    g.addNode("A").addNode("B").addNode("C");
     g.addEdge("A", "B").addEdge("B", "C");
     const { order } = g.bfs("A");
     expect(order).toEqual(["A", "B", "C"]);
   });
 
-  test("dijkstra finds shortest paths", () => {
+  test("shortestPathDijkstra finds shortest path", () => {
     const g = new Graph({ directed: true });
-    g.addNode("A").addNode("B").addNode("C");
-    g.addEdge("A", "B", 1).addEdge("A", "C", 5).addEdge("B", "C", 1);
-    const { distances } = g.dijkstra("A");
-    expect(distances["C"]).toBe(2);
+    g.addEdge("A", "B", { weight: 1 })
+      .addEdge("A", "C", { weight: 5 })
+      .addEdge("B", "C", { weight: 1 });
+    const { distance, path } = g.shortestPathDijkstra("A", "C");
+    expect(distance).toBe(2);
+    expect(path).toEqual(["A", "B", "C"]);
   });
 });
