27cfa74d115f7caa15e3406dd15ca9f815d141fb
Aaron Beavers <foamy125@gmail.com>
2025-08-09 15:35:30 -0500

    fix lint config and heartbeat test

--- shared/py/tests/test_heartbeat_client.py
+++ shared/py/tests/test_heartbeat_client.py
@@ -1,6 +1,10 @@
+import asyncio
 import json
 import os
+import threading
 import sys
+
+import websockets
 
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))
@@ -9,11 +13,16 @@ sys.path.insert(
 from shared.py import heartbeat_client
 
 
+async def handler(_websocket):
+    pass
+
+
 def test_send_once(monkeypatch):
     sent = {}
 
     class DummyConn:
         open = True
+
         def __init__(self, url):
             self.url = url
 
@@ -22,6 +31,7 @@ def test_send_once(monkeypatch):
 
         def close(self):
             pass
+
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
