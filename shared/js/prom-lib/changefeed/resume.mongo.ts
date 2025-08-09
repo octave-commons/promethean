aa96ae32f2fe1baa379c71d074074fadf96d812f
Aaron Beavers <foamy125@gmail.com>
2025-08-09 18:01:33 -0500

    feat: add schema evolution utilities and topic linter

---
+++ shared/js/prom-lib/changefeed/resume.mongo.ts
@@ -0,0 +1,14 @@
+import type { Db, ResumeToken } from "mongodb";
+
+export function tokenStore(db: Db, key = "changefeed:default") {
+  const coll = db.collection<{ _id: string; token: any }>("changefeed_tokens");
+  return {
+    async load(): Promise<ResumeToken | null> {
+      const d = await coll.findOne({ _id: key });
+      return (d?.token as any) ?? null;
+    },
+    async save(token: ResumeToken) {
+      await coll.updateOne({ _id: key }, { $set: { token } }, { upsert: true });
+    },
+  };
+}
