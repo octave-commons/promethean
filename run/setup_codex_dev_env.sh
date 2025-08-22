886dc443026fcbaa79bb3373c1fee12b79ee4b22
Error <foamy125@gmail.com>
2025-08-22 14:55:59 -0500

    hack

--- run/setup_codex_dev_env.sh
+++ run/setup_codex_dev_env.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
+curl -LsSf https://astral.sh/uv/install.sh | sh
+curl -fsSL https://ollama.com/install.sh | sh
+
 npm install --global corepack@latest
 corepack enable pnpm
+
 pip install pytest flake8 black pre-commit hy dotenv
-curl -fsSL https://ollama.com/install.sh | sh
 pre-commit install
 make setup-pipenv
-curl -LsSf https://astral.sh/uv/install.sh | sh
 make setup
