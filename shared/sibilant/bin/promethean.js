a10f6f815047fdd2897aa9003a8638b7a37e82d4
Error <foamy125@gmail.com>
2025-08-24 17:14:27 -0500

    progress

--- shared/sibilant/bin/promethean.js
+++ shared/sibilant/bin/promethean.js
@@ -11,15 +11,15 @@ const __dirname = path.dirname(__filename);
 
 // Map CLI commands to Sibilant entrypoints
 const commands = {
-    repl: path.resolve(__dirname, '../shared/sibilant/src/node/repl/repl.sibilant'),
-    shell: path.resolve(__dirname, '../shared/sibilant/src/node/shell/index.sibilant'),
+    repl: path.resolve(__dirname, '../src/node/repl/repl.sibilant'),
+    shell: path.resolve(__dirname, '../src/node/shell/index.sibilant'),
     compile: path.resolve(__dirname, '../dev/compile.sibilant'),
 };
 
 function runSibilant(file, args = []) {
     // Use dist/lang/cli.js (compiled version of src/lang/cli.sibilant)
     const lithpPath = path.resolve(__dirname, '../shared/sibilant/dist/lang/cli.js');
-    const nodeArgs = [lithpPath, file, ...args];
+    const nodeArgs = [lithpPath, '-x', file, ...args];
 
     const child = spawn('node', nodeArgs, {
         stdio: 'inherit',
