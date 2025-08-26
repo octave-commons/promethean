5b244e6a33341fb3dd6bfcd939d9ea5784c72cb5
Err <foamy125@gmail.com>
2025-08-26 18:52:06 -0500

    refactor: centralize sitecustomize for python services

--- services/py/stt/ecosystem.config.js
+++ services/py/stt/ecosystem.config.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 const root = path.join(__dirname, "../../..");
 
 if (!process.env.PROMETHEAN_ROOT_ECOSYSTEM) {
-  defineApp.PYTHONPATH = root;
+  defineApp.PYTHONPATH = [root, path.join(root, "services/py/shared")].join(":");
   defineApp.HEARTBEAT_PORT = 5005;
 }
 
