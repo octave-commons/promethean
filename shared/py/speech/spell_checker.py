8398a178d30cdcf1519d27655095015b23e6d063
Err <foamy125@gmail.com>
2025-08-26 22:05:50 -0500

    feat: lazy spell checker model

--- shared/py/speech/spell_checker.py
+++ shared/py/speech/spell_checker.py
@@ -1,15 +1,36 @@
 """Spell checker utilities."""
 
+import os
+from typing import Optional, Tuple
+
 from transformers import T5ForConditionalGeneration, T5Tokenizer
 
+_model: Optional[T5ForConditionalGeneration] = None
+_tokenizer: Optional[T5Tokenizer] = None
 
-# Load model and tokenizer once so that calls to ``correct`` are inexpensive.
-model = T5ForConditionalGeneration.from_pretrained("Unbabel/gec-t5_small")
-tokenizer = T5Tokenizer.from_pretrained("t5-small")
+
+def _get_model() -> Tuple[Optional[T5ForConditionalGeneration], Optional[T5Tokenizer]]:
+    """Return cached model and tokenizer, loading on first use."""
+
+    global _model, _tokenizer
+
+    if _model is None or _tokenizer is None:
+        if os.getenv("PROMETHEAN_SKIP_SPELL_MODEL") == "1":
+            return None, None
+
+        _model = T5ForConditionalGeneration.from_pretrained("Unbabel/gec-t5_small")
+        _tokenizer = T5Tokenizer.from_pretrained("t5-small")
+
+    return _model, _tokenizer
 
 
 def correct(sentence: str) -> str:
     """Return the grammar-corrected version of ``sentence``."""
+
+    model, tokenizer = _get_model()
+
+    if model is None or tokenizer is None:
+        return sentence
 
     tokenized_sentence = tokenizer(
         "gec: " + sentence,
