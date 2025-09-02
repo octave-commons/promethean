2eb2f7e699e30cb3a8d0ae2dc4dd284dc0c2583b
Err <foamy125@gmail.com>
2025-09-02 15:38:47 -0500

    chore: add changelog entry for buildfix fixes

--- packages/buildfix/src/iter/git.ts
+++ packages/buildfix/src/iter/git.ts
@@ -1,5 +1,3 @@
-import * as path from "path";
-import { promises as fs } from "fs";
 import { git, isGitRepo, sanitizeBranch } from "../utils.js";
 
 export async function ensureBranch(branch: string) {
