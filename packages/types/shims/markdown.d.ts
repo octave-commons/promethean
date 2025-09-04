ba0901d2f1bbcc2a19b3a0ead1f7194869e96c44
Err <foamy125@gmail.com>
2025-09-04 17:25:53 -0500

    chore(types): centralize markdown shims

---
+++ packages/types/shims/markdown.d.ts
@@ -0,0 +1,4 @@
+declare module '@promethean/markdown/kanban.js';
+declare module '@promethean/markdown/sync.js';
+declare module '@promethean/markdown/task.js';
+declare module '@promethean/markdown/statuses.js';
