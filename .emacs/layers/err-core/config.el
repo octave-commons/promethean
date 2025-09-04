0253b88025cac379cbd5865004813acc06ed892d
Error <foamy125@gmail.com>
2025-09-04 13:16:45 -0500

    updated err-core layer

--- .emacs/layers/err-core/config.el
+++ .emacs/layers/err-core/config.el
@@ -24,70 +24,14 @@
 (with-eval-after-load 'company
   (global-company-mode 1))
 
-;; --- Org / Babel / Python ---
-(with-eval-after-load 'org
-  (setq org-src-fontify-natively t
-        org-src-tab-acts-natively t
-        org-edit-src-content-indentation 0
-        org-confirm-babel-evaluate nil
-        org-startup-with-inline-images t
-        org-babel-python-command "/home/err/.venvs/main/bin/python")
-  (org-babel-do-load-languages
-   'org-babel-load-languages '((python . t)))
-  (add-hook 'org-babel-after-execute-hook #'org-display-inline-images))
 
 ;; Also align inferior python shell
 (setq python-shell-interpreter "/home/err/.venvs/main/bin/python")
 
 ;; --- Markdown fenced code mapping ---
-(with-eval-after-load 'markdown-mode
-  (setq markdown-fontify-code-blocks-natively t)
-  (add-to-list 'markdown-code-lang-modes '("clj"  . clojure-mode))
-  (add-to-list 'markdown-code-lang-modes '("cljs" . clojure-mode))
-  (let* ((ts-mode (cond
-                   ((fboundp 'typescript-ts-mode) 'typescript-ts-mode)
-                   ((fboundp 'typescript-mode)    'typescript-mode)
-                   (t 'js-mode)))
-         (tsx-mode (cond
-                    ((fboundp 'tsx-ts-mode)        'tsx-ts-mode)
-                    ((fboundp 'typescript-ts-mode) 'typescript-ts-mode)
-                    ((fboundp 'typescript-mode)    'typescript-mode)
-                    (t ts-mode))))
-    (dolist (pair `(("ts"  . ,ts-mode)
-                    ("tsx" . ,tsx-mode)
-                    ("bb"  . clojure-mode)
-                    ("babashka" . clojure-mode)
-                    ("clj" . clojure-mode)
-                    ("edn" . clojure-mode)))
-      (add-to-list 'markdown-code-lang-modes pair))))
 (add-to-list 'auto-mode-alist '("\\.bb\\'" . clojure-mode))
 
-;; --- LSP tuning + TS server logging ---
-(with-eval-after-load 'lsp-mode
-  (setq lsp-idle-delay 0.3
-        lsp-file-watch-threshold 5000
-        lsp-completion-enable-additional-text-edit nil))
 
-(with-eval-after-load 'lsp-mode
-  (setq lsp-typescript-tsserver-log 'verbose
-        lsp-typescript-tsserver-trace 'verbose
-        lsp-typescript-initialization-options
-        `(:tsserver
-          (:logDirectory ,(expand-file-name ".lsp-tsserver-logs" user-emacs-directory)
-                         :logVerbosity "verbose"))))
-
-;; --- Copilot indentation guardrails for Lisps ---
-(with-eval-after-load 'copilot
-  (advice-add 'copilot--infer-indentation-offset :around
-              (lambda (orig &rest args)
-                (condition-case _
-                    (apply orig args)
-                  (warning (err/copilot-lisp-indent-fallback))
-                  (error   (err/copilot-lisp-indent-fallback)))))
-  (dolist (m '(emacs-lisp-mode lisp-mode lisp-interaction-mode scheme-mode clojure-mode hy-mode))
-    (add-hook (intern (format "%s-hook" m))
-              (lambda ()
-                (setq-local lisp-body-indent (or (and (numberp lisp-body-indent) lisp-body-indent) 2))
-                (setq-local standard-indent   (or (and (numberp standard-indent) standard-indent) 2))
-                (setq-local tab-width         (or (and (numberp tab-width) tab-width) 2))
-                (setq-local indent-tabs-mode nil)))))
+;; If you used quicklisp-slime-helper, this sets SLIME's path cleanly:
+;; If you need to pin your Lisp explicitly (otherwise SBCL via Roswell is fine):
+;; (setq inferior-lisp-program "ros -Q run")
