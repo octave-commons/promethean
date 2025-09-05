557a86fc004b1beeecf4076178905b9b92fa6b82
Error <foamy125@gmail.com>
2025-09-05 12:43:22 -0500

    Redundent util

--- packages/test-utils/src/sleep.ts
+++
@@ -1,7 +0,0 @@
-export function sleep(ms: number): Promise<void> {
-    return new Promise((resolve) => {
-        const id = setTimeout(resolve, Math.max(0, ms | 0));
-        // In Node, allow the event loop to exit naturally.
-        (id as any).unref?.();
-    });
-}
