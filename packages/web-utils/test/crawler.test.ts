da113d03f24740cf6b6c3915953b73cfdc2f4be3
Err <foamy125@gmail.com>
2025-09-05 18:20:48 -0500

    feat: consolidate web utilities

--- packages/web-utils/test/crawler.test.ts
+++ packages/web-utils/test/crawler.test.ts
@@ -1,6 +1,6 @@
 import test from "ava";
 
-import { crawlPage } from "../src/index.js";
+import { crawlPage } from '../src/crawler.js';
 
 test("crawlPage fetches and extracts links", async (t) => {
   const html = `<html><head><title>Example</title></head><body>
