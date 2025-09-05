f29f710a898300d19293472550ada078ad081b39
Err <foamy125@gmail.com>
2025-09-05 17:51:47 -0500

    chore: address review feedback

--- packages/cephalon/src/actions/set-desktop-channel.scope.ts
+++ packages/cephalon/src/actions/set-desktop-channel.scope.ts
@@ -1,16 +1,18 @@
-import { makePolicy, type PolicyChecker } from "@promethean/security";
 import { checkPermission } from "@promethean/legacy";
-
-import { createLogger, type Logger } from "@promethean/utils/logger.js";
+import { makePolicy, type PolicyChecker } from "@promethean/security";
+import { createLogger, type Logger } from "@promethean/utils";
 
 export type SetDesktopChannelScope = {
-  logger: Logger;
-  policy: PolicyChecker;
+	logger: Logger;
+	policy: PolicyChecker;
 };
 
 export async function buildSetDesktopChannelScope(): Promise<SetDesktopChannelScope> {
-  return {
-    logger: createLogger({ service: "set-desktop-channel" }),
-    policy: makePolicy({ permissionGate: checkPermission }),
-  };
+	return {
+		logger: createLogger({
+			service: "cephalon",
+			base: { component: "set-desktop-channel" },
+		}),
+		policy: makePolicy({ permissionGate: checkPermission }),
+	};
 }
