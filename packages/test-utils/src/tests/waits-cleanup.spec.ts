428febe27791eeaf310344d3dfcafc0535ef0edf
Error <foamy125@gmail.com>
2025-09-06 17:21:49 -0500

    add tests for test utils

---
+++ packages/test-utils/src/tests/waits-cleanup.spec.ts
@@ -0,0 +1,14 @@
+import test from 'ava';
+import { waitForLog } from '../waitForLog.js';
+import { spawn } from 'node:child_process';
+
+test('waitForLog cleans up listeners after resolve', async (t) => {
+    const code = "setTimeout(() => console.log('READY'), 20); setTimeout(() => {}, 200)";
+    const child = spawn('node', ['-e', code], { stdio: 'pipe' });
+
+    await waitForLog(child, /READY/, 1000);
+
+    t.is(child.listenerCount('exit'), 0);
+    t.is(child.stdout?.listenerCount('data') ?? -1, 0);
+    t.is(child.stderr?.listenerCount('data') ?? -1, 0);
+});
