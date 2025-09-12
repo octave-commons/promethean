248ebbf6680740cca2ec3b41ec2c4768784cecfe
Err <foamy125@gmail.com>
2025-09-12 14:40:31 -0500

    chore: add changelog entry

--- packages/discord/src/index.ts
+++ packages/discord/src/index.ts
@@ -1,6 +1,6 @@
-export { handleSocialMessageCreated as indexAttachments } from './attachment-indexer/index.js';
-export { handleSocialMessageCreated as indexMessage } from './message-indexer/index.js';
-export { embedMessage } from './message-embedder/index.js';
-export { GatewayPublisher } from './gateway/gateway.js';
-export { DiscordRestProxy } from './rest/rest.js';
-export { convert } from './embedder/converter.js';
+export { handleSocialMessageCreated as indexAttachments } from "./attachment-indexer/index.js";
+export { handleSocialMessageCreated as indexMessage } from "./message-indexer/index.js";
+export { embedMessage } from "./message-embedder/index.js";
+export { GatewayPublisher } from "./gateway/gateway.js";
+export { DiscordRestProxy, getChannelId } from "./rest/rest.js";
+export { convert } from "./embedder/converter.js";
