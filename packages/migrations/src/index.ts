d76276441ab032576172da8254f039cf1e1749ea
Err <foamy125@gmail.com>
2025-09-13 14:33:28 -0500

    feat(migrations): add embedder and chroma wrappers

--- packages/migrations/src/index.ts
+++ packages/migrations/src/index.ts
@@ -1,5 +1,5 @@
 export * from './checkpoints.js';
 export * from './integrity.js';
 export * from './contract.js';
-// Re-export embedding utilities from @promethean/embedding to keep consumers stable
-export { makeDeterministicEmbedder, assertDim, makeChromaWrapper } from '@promethean/embedding';
+export { makeDeterministicEmbedder, assertDim } from './embedder.js';
+export { makeChromaWrapper } from './chroma.js';
