cc8faddc7e51b1072314ff5bc5f5f7e8320b6b58
Error <foamy125@gmail.com>
2025-09-26 14:15:48 -0500

    Auto stash before merge of "dev/testing" and "origin/dev/testing"

--- scripts/test_paths.py
+++
@@ -1,9 +0,0 @@
-import pytest; pytest.skip("example script", allow_module_level=True)
-import os
-print(os.path.dirname(os.path.abspath(__file__)))
-print(os.getcwd())
-print(__file__)
-print(os.path.abspath("models/public/forward-tacotron/forward-tacotron-duration-prediction/FP16/forward-tacotron-duration-prediction.xml"))
-test_path="models/public/forward-tacotron/forward-tacotron-duration-prediction/FP16/forward-tacotron-duration-prediction.xml"
-print(os.path.exists(test_path))
-
