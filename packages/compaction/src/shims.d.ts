bec42a21ecd807941f357b8dae5cec5cf493aca3
Err <foamy125@gmail.com>
2025-09-30 11:37:57 -0500

    refactor compactor typings

--- packages/compaction/src/shims.d.ts
+++ packages/compaction/src/shims.d.ts
@@ -1,2 +1,27 @@
 declare module '@promethean/event/mongo.js';
-declare module '@promethean/event/types.js';
+
+declare module '@promethean/event/types.js' {
+    import type { ReadonlyDeep } from 'type-fest';
+
+    export type EventRecord<T = unknown> = ReadonlyDeep<
+        {
+            readonly id?: string;
+            readonly ts?: number;
+            readonly topic?: string;
+            readonly key?: string;
+            readonly payload: T;
+            readonly headers?: Record<string, string>;
+        } & Record<string, unknown>
+    >;
+
+    export type PublishOptions = ReadonlyDeep<
+        {
+            readonly key?: string;
+            readonly headers?: Record<string, string>;
+        } & Record<string, unknown>
+    >;
+
+    export type EventBus = {
+        publish<T>(topic: string, payload: ReadonlyDeep<T>, opts?: PublishOptions): Promise<EventRecord<T>>;
+    };
+}
