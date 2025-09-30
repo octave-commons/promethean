93aebe01d44bc01bf8a33807cca0e58f4193f945
Error <foamy125@gmail.com>
2025-09-30 12:22:54 -0500

    add todo tests

---
+++ packages/kanban/src/tests/pull.test.ts
@@ -0,0 +1,3 @@
+import test from "ava";
+test.todo("There are no yaml errors in frontmatter");
+test.todo("Task file bodies are the same before and after the board pull");
--- packages/kanban/src/tests/push.test.ts
+++ packages/kanban/src/tests/push.test.ts
@@ -1,2 +1,3 @@
 import test from "ava";
-test.todo("There are no yaml errors");
+test.todo("There are no yaml errors in frontmatter");
+test.todo("Task file bodies are the same before and after the push");
