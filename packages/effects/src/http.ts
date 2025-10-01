5e4c9f0f80e0c39c25fc1e123668d9bddfd89808
Err <foamy125@gmail.com>
2025-10-01 11:10:30 -0500

    fix: allow readonly request init

--- packages/effects/src/http.ts
+++ packages/effects/src/http.ts
@@ -1,5 +1,5 @@
 import type { ReadonlyDeep } from 'type-fest';
 
 export async function httpFetch(url: string, opts?: ReadonlyDeep<RequestInit>): Promise<Response> {
-    return fetch(url, opts);
+    return fetch(url, opts as unknown as RequestInit | undefined);
 }
