a116f255dff590e7408eb476bda87cebf2d191a2
Err <foamy125@gmail.com>
2025-10-02 16:19:57 -0500

    fix: clean pcm clamp tests

--- packages/duck-audio/test/pcm.test.ts
+++ packages/duck-audio/test/pcm.test.ts
@@ -10,14 +10,3 @@ test("float32ToInt16 maps range", (t) => {
   const out = float32ToInt16(new Float32Array([-1, 0, 1]));
   t.deepEqual(Array.from(out), [-32768, 0, 32767]);
 });
-import { clamp16, float32ToInt16 } from "../src/pcm.js";
-
-test("clamp16 bounds", (t) => {
-  t.is(clamp16(1), 32767);
-  t.is(clamp16(-1), -32768);
-});
-
-test("float32ToInt16 maps range", (t) => {
-  const out = float32ToInt16(new Float32Array([-1, 0, 1]));
-  t.deepEqual(Array.from(out), [-32768, 0, 32767]);
-});
