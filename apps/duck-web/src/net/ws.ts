30b0705495b46e776cf9c8b521e5a64fb64171a8
Err <foamy125@gmail.com>
2025-10-02 15:47:52 -0500

    Update apps/duck-web/src/net/ws.ts

--- apps/duck-web/src/net/ws.ts
+++ apps/duck-web/src/net/ws.ts
@@ -1,1 +1,6 @@
-export const openWs = (url: string, bearer: string) => new WebSocket(url, ['duck.v1', `bearer.${bearer}`]);\n",
+export type WsFactory = (url: string, protocols?: string | string[]) => WebSocket;
+export const openWs = (mkWs: WsFactory = (u,p) => new WebSocket(u,p)) =>
+  (url: string, bearer?: string) => {
+    const protocols = ['duck.v1', ...(bearer ? [`bearer.${bearer}`] : [])];
+    return mkWs(url, protocols);
+  };
