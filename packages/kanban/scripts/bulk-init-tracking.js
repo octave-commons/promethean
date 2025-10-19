7253c51abc76f555313d1c21a3e1d4a31fd00667
Error <foamy125@gmail.com>
2025-10-19 11:27:55 -0500

    update bulk-init-tracking.js to use correct relative path for tasks d...

--- packages/kanban/scripts/bulk-init-tracking.js
+++ packages/kanban/scripts/bulk-init-tracking.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const tasksDir = path.join(__dirname, '../../docs/agile/tasks');
+const tasksDir = path.join(__dirname, '../../../docs/agile/tasks');
 
 console.log('🚀 Fast bulk commit tracking initialization...');
 
