6f3d2b3606d7642cc7d0eeb085804c32cfd01d00
Error <foamy125@gmail.com>
2025-10-20 17:55:56 -0500

    add edge cases and integration tests for move commands with argv vali...

--- packages/kanban/src/tests/move-commands-integration-simple.test.ts
+++ packages/kanban/src/tests/move-commands-integration-simple.test.ts
@@ -16,6 +16,7 @@ test('move_up - multiple moves in sequence', async (t) => {
   const context: CliContext = {
     boardFile: boardPath,
     tasksDir,
+    argv: [],
   };
 
   // Create 5 tasks
@@ -59,6 +60,7 @@ test('move_down - multiple moves in sequence', async (t) => {
   const context: CliContext = {
     boardFile: boardPath,
     tasksDir,
+    argv: [],
   };
 
   // Create 5 tasks
@@ -102,6 +104,7 @@ test('move operations - tasks across different columns', async (t) => {
   const context: CliContext = {
     boardFile: boardPath,
     tasksDir,
+    argv: [],
   };
 
   // Create tasks in different columns
@@ -144,6 +147,7 @@ test('move operations - board persistence', async (t) => {
   const context: CliContext = {
     boardFile: boardPath,
     tasksDir,
+    argv: [],
   };
 
   // Create tasks
@@ -182,6 +186,7 @@ test('move operations - performance with many tasks', async (t) => {
   const context: CliContext = {
     boardFile: boardPath,
     tasksDir,
+    argv: [],
   };
 
   // Create 20 tasks
