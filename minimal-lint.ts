bdf2a472342d7f0d19001af3e5e55e0e03064707
Error <foamy125@gmail.com>
2025-10-21 22:53:29 -0500

    add global variables, classes, and test cases with console logs and e...

--- minimal-lint.ts
+++ minimal-lint.ts
@@ -4,3 +4,22 @@
 var x;
 class Y {}
 let z;
+
+// Adding more violations
+var globalVar1 = 'test1';
+var globalVar2 = 'test2';
+class AnotherClass {}
+class YetAnotherClass {}
+let anotherLet = 'test';
+const mutableArray = [1, 2, 3];
+mutableArray.push(4);
+const mutableObject = { prop: 'value' };
+mutableObject.newProp = 'new';
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+try {
+  console.log('test');
+} catch (error) {
+  console.log(error);
+}
