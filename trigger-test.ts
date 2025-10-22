9c9c1810154aee63a69b41f8bfb4462ee2e83e91
Error <foamy125@gmail.com>
2025-10-21 22:46:38 -0500

    feat: update 7 file(s) [auto]
    - eslint-immediate.ts
    - eslint-realtime.ts
    - functional-violations.ts
    - lint-only-violations.ts
    - no-class-immediate.ts
    - pure-eslint-violations.ts
    - targeted-eslint-violations.ts

---
+++ trigger-test.ts
@@ -0,0 +1,14 @@
+var x;
+class Y {}
+let z;
+const a = [];
+a.push(1);
+const b = {};
+b.c = 2;
+for (let i = 0; i < 5; i++) {}
+try {
+} catch (e) {}
+function f(a, b, c, d, e, f) {}
+const g: any = 1;
+require('fs');
+module.exports = {};
