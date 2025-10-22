5d4c381b76fa663566b9210fe68d32988e4162f1
Error <foamy125@gmail.com>
2025-10-21 21:42:02 -0500

    add eslint config for legacy require and module exports support in pe...

---
+++ packages/persistence/eslint.config.js
@@ -0,0 +1,23 @@
+import baseConfig from '../../eslint.config.ts';
+
+export default [
+    ...baseConfig,
+    {
+        files: ['src/**/*.ts'],
+        rules: {
+            // Allow class declarations for legacy compatibility
+            'no-restricted-syntax': [
+                'error',
+                {
+                    selector: "CallExpression[callee.name='require']",
+                    message: 'ESM only',
+                },
+                {
+                    selector: "MemberExpression[object.name='module'][property.name='exports']",
+                    message: 'ESM only',
+                },
+                // Remove ClassDeclaration and ClassExpression restrictions
+            ],
+        },
+    },
+];
