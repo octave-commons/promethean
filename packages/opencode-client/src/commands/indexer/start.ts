fc0f50eb033e66a5412a61ea4dc5c8db7d874c06
Error <foamy125@gmail.com>
2025-10-22 22:00:47 -0500

    remove unused imports and execAsync promisify
    - remove unused initializeStores import
    - remove unused execAsync promisify
    - reduce code bloat and improve maintainability

--- packages/opencode-client/src/commands/indexer/start.ts
+++ packages/opencode-client/src/commands/indexer/start.ts
@@ -3,9 +3,6 @@
 import { exec } from 'child_process';
 import { promisify } from 'util';
 import { createIndexerService } from '../../services/indexer.js';
-import { initializeStores } from '../../initializeStores.js';
-
-const execAsync = promisify(exec);
 
 export async function main() {
   try {
