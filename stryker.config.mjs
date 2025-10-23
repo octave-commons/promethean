91a72b6351547a646ae76493b1806ff9dbd30010
Error <foamy125@gmail.com>
2025-10-23 14:07:16 -0500

    add @stryker-mutator/typescript v4.0.0 and configure mutator with tsc...
    - specify tsconfig.json as config file location
    - set typescript as mutator and transpiler
    - ensure typescript code is transpiled before test execution

--- stryker.config.mjs
+++ stryker.config.mjs
@@ -11,5 +11,11 @@ const config = {
   coverageAnalysis: 'perTest',
   buildCommand: 'pnpm build',
   plugins: ['@stryker-mutator/tap-runner'],
+  tsconfigFile: 'tsconfig.json', // Location of your tsconfig.json file
+  mutator: 'typescript', // Specify that you want to mutate typescript code
+  transpilers: [
+    'typescript' // Specify that your typescript code needs to be transpiled before tests can be run. Not needed if you're using ts-node Just-in-time compilation.
+]
+
 };
 export default config;
