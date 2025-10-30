90ae4f2f4da2e11a6056596d4afb968f178c33f5
Error <foamy125@gmail.com>
2025-10-30 18:41:12 -0500

    add test rules for ClojureScript interop with task movement validation
    - introduces test-eval function to validate task movement from "todo" to "in-progress"
    - exports test-eval-js for ClojureScript interop
    - ensures task title and source/destination match are checked

---
+++ packages/kanban/test-rules.clj
@@ -0,0 +1,11 @@
+(ns test-rules
+  "Test rules for ClojureScript interop")
+
+(defn test-eval [from to task board]
+  ;; Simple test function that returns true if task has a title
+  (and (:title task)
+       (= from "todo")
+       (= to "in-progress")))
+
+(defn ^:export test-eval-js [from to task-js board-js]
+  (test-eval from to task-js board-js))
