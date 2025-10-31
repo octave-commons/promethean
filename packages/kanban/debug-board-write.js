e3a2676eab88d0e173bd34aae8089968748655a9
Error <foamy125@gmail.com>
2025-10-30 20:33:38 -0500

    fix debug-board-write: add readFile import for board state reading

--- packages/kanban/debug-board-write.js
+++ packages/kanban/debug-board-write.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import path from 'node:path';
-import { writeFile, mkdir } from 'node:fs/promises';
+import { writeFile, mkdir, readFile } from 'node:fs/promises';
 import { writeBoard } from './dist/lib/serializers/board.js';
 
 async function testBoardWrite() {
