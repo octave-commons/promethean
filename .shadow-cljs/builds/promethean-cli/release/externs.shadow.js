fb2d84f6957660efbd5fad9be60edacbcb1ef10f
err <err@promethean-staging-laptop.tailbe888a.ts.net>
2025-11-01 08:16:13 -0500

    not sure

---
+++ .shadow-cljs/builds/promethean-cli/release/externs.shadow.js
@@ -0,0 +1,9 @@
+/** @const {ShadowJS} */ var module;
+ShadowJS.prototype.basename;
+ShadowJS.prototype.existsSync;
+ShadowJS.prototype.exit;
+ShadowJS.prototype.on;
+ShadowJS.prototype.parse;
+ShadowJS.prototype.readFileSync;
+ShadowJS.prototype.readdirSync;
+ShadowJS.prototype.spawn;
---
+++ packages/mcp/dev-ui/.shadow-cljs/builds/dev-ui/release/externs.shadow.js
@@ -0,0 +1,6 @@
+/** @const {ShadowJS} */ var Promise;
+/** @const {ShadowJS} */ var customElements;
+/** @const {ShadowJS} */ var fetch;
+/** @const {ShadowJS} */ var module;
+ShadowJS.prototype.reject;
+ShadowJS.prototype.stringify;
