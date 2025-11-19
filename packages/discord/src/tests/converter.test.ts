09cc92c0ce33b1e7ee1c3a83d8a70a18e38b4fec
Error <foamy125@gmail.com>
2025-11-19 11:30:57 -0600

    Remove improperly nested repo packages/discord

--- packages/discord/src/tests/converter.test.ts
+++
@@ -1,9 +0,0 @@
-import test from "ava";
-import { convert } from "@promethean-os/discord";
-
-const sample = Buffer.from("sample");
-
-test("convert ogg stream to wav stream", (t) => {
-  const result = convert(sample);
-  t.true(result.equals(sample));
-});
